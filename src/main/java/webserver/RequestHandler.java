68892bd3665445d6438cc10c97408228a296be6c
jongs <jongseo12345@gmail.com>
2024-07-02 14:35:00 +0900

    fix: requestHandler - 다양한 컨텐츠 지원
    기존 지원 컨텐츠
    - html
    
    수정 후 지원되는 컨텐츠
    - html, css, js, ico, png, jpg

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -37,14 +37,15 @@ public class RequestHandler implements Runnable {
 
                 DataOutputStream dos = new DataOutputStream(out);
                 if ("/".equals(url)) {
-                    response200Header(dos, "Hello World!".length());
-                    responseBody(dos, "Hello World!".getBytes());
+                    response200Header(dos, "<html><body><h1>Hello World!</h1></body></html>".length(), "text/html");
+                    responseBody(dos, "<html><body><h1>Hello World!</h1></body></html>".getBytes());
                 } else {
                     File file = new File("src/main/resources/static" + url);
 
                     if (file.exists() && !file.isDirectory()) {
                         byte[] body = readFileToByteArray(file);
-                        response200Header(dos, body.length);
+                        String contentType = getContentType(url);
+                        response200Header(dos, body.length, contentType);
                         responseBody(dos, body);
                     } else {
                         response404Header(dos);
@@ -66,11 +67,28 @@ public class RequestHandler implements Runnable {
             return null;
         }
     }
+    private String getContentType(String url) {
+        if (url.endsWith(".html")) {
+            return "text/html";
+        } else if (url.endsWith(".css")) {
+            return "text/css";
+        } else if (url.endsWith(".js")) {
+            return "application/javascript";
+        } else if (url.endsWith(".ico")) {
+            return "image/x-icon";
+        } else if (url.endsWith(".png")) {
+            return "image/png";
+        } else if (url.endsWith(".jpg") || url.endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else {
+            return "application/octet-stream";
+        }
+    }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
